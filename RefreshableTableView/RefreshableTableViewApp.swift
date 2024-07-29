c129e24ccc0bed115086674dd2b8420932b5722e
Cydiater <cydiater@gmail.com>
2024-07-29 22:37:16 +0800

    Initial Commit

---
+++ RefreshableTableView/RefreshableTableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshableTableViewApp.swift
+//  RefreshableTableView
+//
+//  Created by Cydiater on 29/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct RefreshableTableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
